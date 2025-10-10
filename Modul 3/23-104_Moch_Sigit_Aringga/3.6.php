caaa93228d0ffc66971cbdb28c8726c86f2d9594
Ari <aringga098@.gmailcom>
2025-10-10 18:33:32 +0700

    tugas 3 edit

--- Modul 3/23-104_Moch_Sigit_Aringga/3.6.php
+++ Modul 3/23-104_Moch_Sigit_Aringga/3.6.php
@@ -42,32 +42,45 @@ print_r($filtered);
 
 
 echo "<br>";
-echo "<br>";
+
 echo "nomor 6 ";
 echo "<br>";
 $sort_example = array("Orange", "Apple", "Banana");
+echo '<br>';
 
 sort($sort_example);
 echo 'short a-z';
+echo '<br>';
 echo "sort(): "; print_r($sort_example);
+echo '<br>';
+echo '<br>';
 
 rsort($sort_example);
 echo 'short z-a';
+echo '<br>';
 echo "<br>rsort(): "; print_r($sort_example);
-
+echo '<br>';
 $assoc_array = array("b"=>"Lemon", "a"=>"Orange", "c"=>"Banana");
 
+echo '<br>';
 asort($assoc_array);
 echo'short a-z tetap key';
+echo '<br>';
 echo "<br>asort(): "; print_r($assoc_array);
+echo "<br>";
 
 ksort($assoc_array);
 echo 'short a-z key';
+echo '<br>';
 echo "<br>ksort(): "; print_r($assoc_array);
+echo "<br>";
 
+echo "<br>";
 arsort($assoc_array);
 echo 'short z-a tetap key';
+echo '<br>';
 echo "<br>arsort(): "; print_r($assoc_array);
+echo "<br>";
 
 krsort($assoc_array);
 echo 'short z-a key';
